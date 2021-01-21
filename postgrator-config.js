1340bac399c8911b6e3b410274af1d9628df3932
Jessica <jmbaskerville1217@gmail.com>
2021-01-21 16:12:18 -0500

    added requirements

--- postgrator-config.js
+++ postgrator-config.js
@@ -5,5 +5,5 @@ module.exports = {
     "migrationsDirectory": "migrations",
     "driver": "pg",
     "connectionString": process.env.DATABASE_URL,
-
+    ssl: process.env.NODE_ENV === "production"
 }
