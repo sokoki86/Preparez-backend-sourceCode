5bb255541314e4c37b9a305395ef6eba0d22a994
Jessica <jmbaskerville1217@gmail.com>
2021-01-21 14:56:20 -0500

    changed ssl

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const pg = require("pg"); pg.defaults.ssl = true;
+// const pg = require("pg"); pg.defaults.ssl = true;
 
 module.exports = {
     "migrationsDirectory": "migrations",
